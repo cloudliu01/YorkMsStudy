df36e6250b8600ab83c298f97e8d59c81fb65664
Lizhuang Liu <ll1965@its.york.ac.uk>
2020-09-17 08:18:45 +0100

    Finished Week2 lab4 exercise 1

--- Algorithms and Data Structures/Week2 Labs/Lab4/Twitchers/BirdTable.java
+++ Algorithms and Data Structures/Week2 Labs/Lab4/Twitchers/BirdTable.java
@@ -16,7 +16,27 @@ public class BirdTable {
    {
       //Declare and build an array interactively - see the Temperature Project
    
-      
+	      //Build an array.
+	      int hArray[] = { 19, 15, 7, 11, 9, 13, 5};
+
+	     //String to hold output
+	      String output = "Element\tValue\tHistogram";
+
+	      // for each array element, output a bar in histogram
+	      for ( int counter = 0; counter < hArray.length; counter++ ) {
+	         output += 
+	            "\n" + counter + "\t" + hArray[ counter ] + "\t";
+
+	         // print bar of asterisks
+	         for ( int stars = 0; stars < hArray[ counter ]; stars++ )
+	            output += "*";
+	      }
+	      //Create a new text area object to hold the text
+	      JTextArea outputArea = new JTextArea();
+	      outputArea.setText( output );
+
+	      JOptionPane.showMessageDialog( null, outputArea,
+	         "Histogram Printing Program",JOptionPane.INFORMATION_MESSAGE );
       
       //Display Output using a histogram of '*' - see Histogram Project
    
